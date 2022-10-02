6fbf257931d84b467bd09d7c983e930f2dfbc42e
FelipeEd <felipe.eduardo.dos@gmail.com>
2022-10-01 23:47:28 -0300

    Multiply

--- lib14bis/processing/enhance.py
+++ lib14bis/processing/enhance.py
@@ -1,11 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 
 
 class ImageMock:
     def __init__(self, name):
         self.data = cv2.imread(name)
+
+    def __repr__(self):
+        print(self.data)
 
     def show(self):
         aux = cv2.cvtColor(self.data, cv2.COLOR_BGR2RGB)
@@ -13,9 +17,32 @@ class ImageMock:
         plt.plot()
         # cv.imshow("Mock img", self.data) # não funciona com jupyter
 
+    def copy(self):
+        return copy.deepcopy(self)
+
+    @property
+    def b(self):
+        return self.data[:, :, 0]
+
+    @b.setter
+    def b(self, newValues):
+        self.data[:, :, 0] = newValues
+
+    @property
+    def g(self):
+        return self.data[:, :, 1]
+
+    @g.setter
+    def g(self, newValues):
+        self.data[:, :, 1] = newValues
+
     @property
     def r(self):
-        return self.data[:, :, 0]
+        return self.data[:, :, 2]
+
+    @r.setter
+    def r(self, newValues):
+        self.data[:, :, 2] = newValues
 
     def save(self, name):
         cv2.imwrite(name, self.data)
@@ -38,13 +65,13 @@ class Enhance:
         return cv2.filter2D(src=img.data, ddepth=-1, kernel=kernel)
 
     def multiply(img, facs):
-        new_img = img.copy()
 
+        new_img = img.copy()
         # red
-        img.data.r *= facs[0]
+        new_img.r = np.floor(img.r * facs[0])
         # green
-        img.data.g *= facs[1]
+        new_img.g = np.floor(img.g * facs[1])
         # blue
-        img.data.b *= facs[2]
+        new_img.b = np.floor(img.b * facs[2])
 
         return new_img
